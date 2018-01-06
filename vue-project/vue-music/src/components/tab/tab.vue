b44bbc26c0704b3de827e5bde7fe8b34a005c9f2
zhanghuiqi205 <649676852@qq.com>
2018-01-06 22:05:07 +0800

    路由的设置

--- vue-project/vue-music/src/components/tab/tab.vue
+++ vue-project/vue-music/src/components/tab/tab.vue
@@ -16,10 +16,6 @@
   </div>
 </template>
 
-<script type="text/ecmascript-6">
-  export default {}
-</script>
-
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
   .tab
--- vue-project/vue-music/src/router/index.js
+++ vue-project/vue-music/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Recommend from 'components/recommend/recommend'
-import Singer from 'components/singer/signer'
+import Singer from 'components/singer/singer'
 import Rank from 'components/rank/rank'
 import Search from 'components/search/search'
 
